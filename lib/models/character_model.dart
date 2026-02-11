d1adb0e43af9fcc24b75fd25aeda3663fc0ebd58
Maxim Lyubar <lyubar05@bk.ru>
2026-02-11 17:33:03 +0300

    feat: add offline caching with Hive

--- lib/models/character_model.dart
+++ lib/models/character_model.dart
@@ -1,8 +1,24 @@
+import 'package:hive/hive.dart';
+
+part 'character_model.g.dart';
+
+
+@HiveType(typeId: 0)
 class Character {
+
+  @HiveField(0)
   final int id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final String status;
+
+  @HiveField(3)
   final String species;
+
+  @HiveField(4)
   final String image;
 
   Character({
@@ -17,7 +33,7 @@ class Character {
     return Character(
         id: json['id'],
         name: json['name'],
-        status: json['name'],
+        status: json['status'],
         species: json['species'],
         image: json['image'],
     );
